2b587fbde0c6ef217553b6aa5f640051429dcbd4
Jared Hardy <jared@jaredhardy.com>
2013-01-05 21:42:44 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,104 @@
+function MeetupCtrl($scope) {
+
+	$scope.meetups = [
+		{
+			'title': 				'Intro to Functional Programming',
+			'speaker': 			{
+											'name': 'Joel Holdbrooks', 
+											'url': 'https://github.com/noprompt',
+											},
+			'date': 				{
+											'month': 'Jan', 
+											'day': '08', 
+											'year' : '2013',
+											'time' : '7-8pm'
+											},
+			'location': 		{
+											'name' : 'The Hash Tag', 
+											'mapURL': 'http://www.google.com/maps'
+											},
+			'description': 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent convallis, elit dapibus scelerisque tincidunt, sem nulla pellentesque justo, in convallis leo leo eget nunc. Nullam tempor pretium risus, a porta mi pellentesque ac. Nulla iaculis luctus enim eget scelerisque. Curabitur auctor elit.',
+			'slides': 			''
+		},
+				{
+			'title': 				'Intro to Functional Programming',
+			'speaker': 			{
+											'name': 'Joel Holdbrooks', 
+											'url': 'https://github.com/noprompt',
+											},
+			'date': 				{
+											'month': 'Jan', 
+											'day': '08', 
+											'year' : '2013',
+											'time' : '7-8pm'
+											},
+			'location': 		{
+											'name' : 'The Hash Tag', 
+											'mapURL': 'http://www.google.com/maps'
+											},
+			'description': 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent convallis, elit dapibus scelerisque tincidunt, sem nulla pellentesque justo, in convallis leo leo eget nunc. Nullam tempor pretium risus, a porta mi pellentesque ac. Nulla iaculis luctus enim eget scelerisque. Curabitur auctor elit.',
+			'slides': 			''
+		},
+				{
+			'title': 				'How we messed up a large project using python',
+			'speaker': 			{
+											'name': 'Joel Holdbrooks', 
+											'url': 'https://github.com/noprompt',
+											},
+			'date': 				{
+											'month': 'Jan', 
+											'day': '08', 
+											'year' : '2013',
+											'time' : '7-8pm'
+											},
+			'location': 		{
+											'name' : 'The Hash Tag', 
+											'mapURL': 'http://www.google.com/maps'
+											},
+			'description': 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent convallis, elit dapibus scelerisque tincidunt, sem nulla pellentesque justo, in convallis leo leo eget nunc. Nullam tempor pretium risus, a porta mi pellentesque ac. Nulla iaculis luctus enim eget scelerisque. Curabitur auctor elit.',
+			'slides': 			''
+		},
+				{
+			'title': 				'Intro to Functional Programming',
+			'speaker': 			{
+											'name': 'Joel Holdbrooks', 
+											'url': 'https://github.com/noprompt',
+											},
+			'date': 				{
+											'month': 'Jan', 
+											'day': '08', 
+											'year' : '2013',
+											'time' : '7-8pm'
+											},
+			'location': 		{
+											'name' : 'The Hash Tag', 
+											'mapURL': 'http://www.google.com/maps'
+											},
+			'description': 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent convallis, elit dapibus scelerisque tincidunt, sem nulla pellentesque justo, in convallis leo leo eget nunc. Nullam tempor pretium risus, a porta mi pellentesque ac. Nulla iaculis luctus enim eget scelerisque. Curabitur auctor elit.',
+			'slides': 			''
+		},
+				{
+			'title': 				'Intro to Functional Programming',
+			'speaker': 			{
+											'name': 'Joel Holdbrooks', 
+											'url': 'https://github.com/noprompt',
+											},
+			'date': 				{
+											'month': 'Jan', 
+											'day': '08', 
+											'year' : '2013',
+											'time' : '7-8pm'
+											},
+			'location': 		{
+											'name' : 'The Hash Tag', 
+											'mapURL': 'http://www.google.com/maps'
+											},
+			'description': 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent convallis, elit dapibus scelerisque tincidunt, sem nulla pellentesque justo, in convallis leo leo eget nunc. Nullam tempor pretium risus, a porta mi pellentesque ac. Nulla iaculis luctus enim eget scelerisque. Curabitur auctor elit.',
+			'slides': 			''
+		}
+	];
+
+	this.getIndex = function(index) {
+			console.log(index)
+		}
+}
