5135cf0a012a34806bf306970eacd0094c49372e
Joel Holdbrooks <cjholdbrooks@gmail.com>
2013-02-11 15:55:14 -0800

    Create new meetup model and update handler

--- src/clj/undefined/handler.clj
+++ src/clj/undefined/handler.clj
@@ -1,10 +1,14 @@
 (ns undefined.handler
-  (:use compojure.core)
+  (:use compojure.core
+        ring.middleware.resource
+        ring.middleware.file-info
+        hiccup.middleware)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.util.response :as resp]
             [clojure.data.json :as json]
-            [undefined.models.member :as member]))
+            [undefined.models.member :as member]
+            [undefined.models.meetup :as meetup]))
 
 (defn json-response [data]
   {:status 200
@@ -12,8 +16,8 @@
    :body (json/write-str data)})
 
 (defroutes api-routes
-  (GET "/members.json" []
-    (json-response member/all)))
+  (GET "/members.json" [] (json-response member/all))
+  (GET "/meetups.json" [] (json-response meetup/all)))
 
 (defroutes app-routes
   ;; SEE: api-routes
@@ -28,3 +32,9 @@
 
 (def app
   (handler/site app-routes))
+
+(def war-handler
+  (-> app
+    (wrap-resource "public")
+    (wrap-base-url)
+    (wrap-file-info)))
---
+++ src/clj/undefined/models/meetup.clj
@@ -0,0 +1,20 @@
+(ns undefined.models.meetup
+  (:use [clj-time.core :only [date-time]])
+  (:require [undefined.models.member :as member]))
+
+(def all
+  [{:title "Using Clojure"
+    :description "Clojure is a dynamic, functional programming language built for concurrency and runs on the JVM. It is an incredibly well designed language and emphasizes practicality and simplicity. Joel discusses the rational behind the language, motivations for using it, and how to get started."
+    :slides nil
+    :date (str (date-time 2013 2 13 19))
+    :location {:name "The Hash Tag"
+               :map "https://maps.google.com/maps?q=fresno+hashtag&ie=UTF-8&ei=6GD0UN7ZOOHf2QWDooHgDg&ved=0CAsQ_AUoAA"}
+    :speakers (member/find-by-name "Joel Holdbrooks")}
+   {:title "Hacking with Vim"
+    :description "Justin and Joel discuss one of the oldest and most powerful text editors available today - Vim. Discover the history, philosophy, and potential behind this amazing editor."
+    :slides nil
+    :date (str (date-time 2013 1 16 19))
+    :location {:name "The Hash Tag"
+               :map "https://maps.google.com/maps?q=fresno+hashtag&ie=UTF-8&ei=6GD0UN7ZOOHf2QWDooHgDg&ved=0CAsQ_AUoAA"}
+    :speakers (member/find-by-name "Justin Gable" "Joel Holdbrooks")}])
+
